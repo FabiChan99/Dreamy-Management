934d41b8d8f222d51bcbe2b2f59bb2adcb864110
Fabi-Chan <fabi@fabi-chan.me>
2023-12-10 18:11:46 +0100

    Add soundboard toggle feature to voice channels
    This update introduces the ability to enable or disable a soundboard in voice channels, along with related permission settings. The TempVoiceHelper and Voice.cs files have received substantial modifications to incorporate these changes. Lastly, an entry related to the Dreamy-Management executable has been added to the .gitignore file.

--- src/Commands/TempVC/TempVoice.cs
+++ src/Commands/TempVC/TempVoice.cs
@@ -918,6 +918,42 @@ public class TempVoiceCommands : TempVoiceHelper
             }
         );
     }
+    
+        [Command("togglesoundboard")]
+        [Aliases("vcsoundboard")]
+        public async Task ToggleVcSoundboard(CommandContext ctx)
+        {
+            List<long> dbChannels = await GetChannelIDFromDB(ctx);
+            DiscordChannel userChannel = ctx.Member?.VoiceState?.Channel;
+    
+            bool isMod = await IsChannelMod(userChannel, ctx.Member);
+    
+            if (userChannel == null || !dbChannels.Contains((long)userChannel?.Id) && !isMod)
+            {
+                await NoChannel(ctx);
+                return;
+            }
+    
+            if (userChannel != null && dbChannels.Contains((long)userChannel.Id) || userChannel != null && isMod)
+            {
+                var msg = await ctx.RespondAsync("Status des Soundboards wird geändert");
+                bool SBState = GetSoundboardState(userChannel);
+                if (SBState)
+                {
+                    await SetSoundboardState(userChannel, false);
+                    await msg.ModifyAsync(
+                        "<:success:1085333481820790944> **Erfolg!** Das Soundboard ist nun **deaktiviert**!");
+                    return;
+                }
+    
+                if (!SBState)
+                {
+                    await SetSoundboardState(userChannel, true);
+                    await msg.ModifyAsync(
+                        "<:success:1085333481820790944> **Erfolg!** Das Soundboard ist nun **aktiviert**!");
+                }
+            }
+        }
 
     [Command("permit")]
     [RequireDatabase]
@@ -1329,6 +1365,7 @@ public class TempVoiceCommands : TempVoiceHelper
                             $"> ``{prefix}vckick @user/id`` - Kickt einen User aus dem Channel¹\n" +
                             $"> ``{prefix}block @user/id`` - Blockt einen User aus dem Channel¹\n" +
                             $"> ``{prefix}claim`` - Claimt den aktuellen Channel, wenn sich der Besitzer nicht mehr im Channel befindet\n" +
+                            $"> ``{prefix}togglesoundboard`` - Aktiviert oder Deaktiviert das VC Soundboard\n" +
                             $"> ``{prefix}unblock @user/id`` - Macht die Blockierung eines Users rückgängig¹\n" +
                             $"> ``{prefix}transfer @user/id`` - Transferiert den Channeleigentümer\n" +
                             $"> ``{prefix}permit @user/id`` - Whitelistet einen User für einen Channel¹\n" +
